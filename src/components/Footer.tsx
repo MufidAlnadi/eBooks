68d3bb69589de281fd114cad8f98c3a4d0c8cb6c
MufidAlnadi <126170946+MufidAlnadi@users.noreply.github.com>
2023-05-05 14:17:32 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 py-4 absolute bottom-0 w-full">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-white">© 2023 ChatGPT. All rights reserved.</div>
+        <div className="text-white">© 2023 All rights reserved.</div>
         <div className="flex items-center space-x-4">
           <a href="#" className="text-gray-400 hover:text-gray-300">
             <FaFacebook />
